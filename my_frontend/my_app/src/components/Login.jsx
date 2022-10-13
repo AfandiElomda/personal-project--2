f24c80f871ef4ca6dfbeb6f8d4df1053c9c724c7
+ <+>
2022-10-13 19:01:46 +0300

    Google login properties

--- my_frontend/my_app/src/components/Login.jsx
+++ my_frontend/my_app/src/components/Login.jsx
@@ -16,7 +16,28 @@ const Login = () => {
           controls={false}
           muted
           autoPlay
+          className="w-full h-full object-cover"
         />
+
+        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
+         <div className="p-5">
+          <img src={logo} width="130px" alt="logo"/>
+          </div>
+          <div className="shadow-2xl">
+            <GoogleLogin
+              clientId=''
+              render={(renderProps) => (
+                <button
+                type="button"
+                className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}>
+                   <FcGoogle className="mr-4"/> Sign in with Google
+                </button>
+              )}
+            />
+          </div>
+        </div>
       </div>
     </div>
   )
