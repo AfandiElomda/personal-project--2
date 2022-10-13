b2ac87592403fa28c513366957f50a3e59606878
+ <+>
2022-10-13 19:35:33 +0300

    changed routing

--- my_frontend/my_app/src/App.js
+++ my_frontend/my_app/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react'
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
 
 import Login from './components/Login';
 import Home from './container/Home';
 
 const App = () => {
   return (
+    <BrowserRouter>
     <Routes>
       <Route path="login" element={<Login />} />
       <Route path="/*" element={<Home />} />
     </Routes>
+    </BrowserRouter>
   )
 }
 
